5b249ba6d12f1afcf7ae68942ff7f52ffe03a0ac
Igor Yoshevski <gigo_j44@hotmail.com>
2021-06-29 15:16:21 +0200

    Updates in products, minor tweaks and fix

--- app2/src/components/partials/order-item.js
+++ app2/src/components/partials/order-item.js
@@ -6,7 +6,7 @@ export default function OrderItem(props)
 		<td>{props.id}</td>
 		<td><NavLink exact to={`/order/${props.id}`}>{props.order}</NavLink></td>
 		<td>{props.sum} $</td>
-		<td>{props.tax} $</td>
+		<td>{props.tax} %</td>
 		<td>{props.total} $</td>
 		<td>{props.items.length}</td>
 		<td>{props.status}</td>
